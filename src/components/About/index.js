2fbd9841ae408d0b74a94e61b14878320d88c206
svei00 <79055634+svei00@users.noreply.github.com>
2022-09-17 16:28:48 -0700

    about page finished

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,12 +1,29 @@
 import './index.scss';
 import AnimatedLetters from '../AnimatedLetters';
+import { useEffect, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngular, faCss3, faGitAlt, faHtml5, faJsSquare, faReact } from '@fortawesome/free-brands-svg-icons';
 
 const About = () => {
+
+    const [letterClass, setLetterClass] = useState('text-animate')
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 3000)
+
+        return () => {
+            clearTimeout();
+        }
+    }, []);
+
     return (
         <div className='container about-page'>
             <div className='text-zone'>
                 <h1>
                     <AnimatedLetters 
+                        letterClass={letterClass}
                         strArray={['A', 'b', 'o','u','t', ' ','M','e']}
                         idx={15}                                            // It means 1.5 seconds
                     />
@@ -26,6 +43,31 @@ const About = () => {
                     and wife. We love to travel.
                 </p>
             </div>
+
+            <div className='stage-cube-cont'>
+                <div className='cubespinner'>
+                    <div className='face1'>
+                        <FontAwesomeIcon icon={faAngular} color='#DD0031' />
+                    </div>
+                    <div className='face2'>
+                        <FontAwesomeIcon icon={faHtml5} color='#F06529' />
+                    </div>
+                    <div className='face3'>
+                        <FontAwesomeIcon icon={faCss3} color='#28A4D9' />
+                    </div>
+                    <div className='face4'>
+                        <FontAwesomeIcon icon={faJsSquare} color='#EFD81D' />
+                    </div>
+                    <div className='face5'>
+                        <FontAwesomeIcon icon={faReact} color='#5ED4F4' />
+                    </div>
+                    <div className='face6'>
+                        <FontAwesomeIcon icon={faGitAlt} color='#EC4D28 ' />
+                    </div>
+                </div>
+            </div>
+
+
         </div>
     )
 }
