ff1e4ef5ec28cf1cc91c9732c49ddf77cc2ed704
svei00 <79055634+svei00@users.noreply.github.com>
2022-09-18 20:54:50 -0700

    Added email form and map

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,10 +1,13 @@
 import Loader from 'react-loaders';
 import './index.scss';
 import AnimatedLetters from '../AnimatedLetters';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 
 const Contact = () => {
     const [letterClass, setLetterclass] = useState('text-animate');
+    const refForm = useRef()
 
     useEffect(() => {
         setTimeout(() => {
@@ -15,6 +18,27 @@ const Contact = () => {
             clearTimeout();
         })
     },[])
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs
+            .sendForm(
+                'svei00',
+                'template_f2vnjd2',
+                refForm.current,
+                'Ff-0XRmnMhpWt9tDR'
+            )
+            .then (
+                () => {
+                    alert('Message Successfully sent!!')
+                    window.location.reload(false)
+                },
+                () => {
+                    alert('Failed to send the message, please try again.')
+                }
+            )
+    }
 
     return (
         <>
@@ -35,7 +59,7 @@ const Contact = () => {
                     </p>
                     <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
                     <div className='contact-form'>
-                        <form>
+                        <form ref={refForm} onSubmit={sendEmail}>
                             <ul>
                                 <li className='half'>
                                     <input
@@ -79,6 +103,22 @@ const Contact = () => {
                             </ul>
                         </form>
                     </div>
+                </div>
+                <div className='info-map'>
+                    Iv&aacute;n E. Villanueva.
+                    <br />
+                    United Stetes,
+                    <br />
+                    San Bernardino, CA. 92405.
+                    <span>svei00@gmail.com</span>
+                </div>
+                <div className='map-wrap'>
+                    <MapContainer center={[44.96366, 19.61045]} zoom={13}>
+                        <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
+                        <Marker position={[44.96366, 19.61045]}>
+                            <Popup>Iv&aacute;n lives here!!</Popup>
+                        </Marker>
+                    </MapContainer>
                 </div>
             </div>
             <Loader type='pacman' />
