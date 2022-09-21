7ba4746e8eaf316c0b2f3235daf46aecd6f7d5ab
svei00 <79055634+svei00@users.noreply.github.com>
2022-09-20 19:42:18 -0700

    Addend images and hooks to the portfolio section

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,11 +1,50 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Loader from 'react-loaders';
 import AnimatedLetters from '../AnimatedLetters';
 import './index.scss'
+import portfolioData from '../../data/portfolio.json'
 
 const Portfolio = () => {
 
     const [letterClass, setLetterClass] = useState('text-animate');
+
+    useEffect (() => {
+        const timer = setTimeout(() => {
+            setLetterClass('text-animate-hover');
+        }, 3000);
+
+        return () => {
+            clearTimeout(timer);
+        }
+    })
+
+    const renderPortfolio = (portfolio) => {
+        return (
+            <div className='images-container'>
+                {
+                    portfolio.map((port, idx) => {
+                        return (
+                            <div className='image-box' key={idx}>
+                                <img 
+                                    src={port.cover}
+                                    classname="portfolio-image"
+                                    alt="portfolio"
+                                />
+                                <div className='content'>
+                                    <p className='title'>{port.title}</p>
+                                    <h4 className='description'>{port.description}</h4>
+                                    <button
+                                        className='btn'
+                                        onClick={() => window.open(port.url)}
+                                    >View</button>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        );
+    } 
     
     return (
         <>
@@ -14,9 +53,12 @@ const Portfolio = () => {
                     <AnimatedLetters 
                         letterClass={letterClass}
                         strArray={'Portfolio'.split('')}
-                        idx={15}                            // Remember that 15 means 1.5 and it is the delay from starting loading the page
+                        idx={15}                                                // Remember that 15 means 1.5 and it is the delay from starting loading the page
                     />
                 </h1>
+                <div>
+                    {renderPortfolio(portfolioData.portfolio)}                     {/* We use this mock since we don't have a real databasa */}
+                </div>
             </div>
             <Loader type='pacman' />
         </>
