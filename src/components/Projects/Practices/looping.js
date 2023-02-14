4cb8ed429f8eac85629a3f7902c9df35ce3dc489
svei00 <79055634+svei00@users.noreply.github.com>
2023-02-13 21:43:59 -0800

    adding chessboard and Fizzbuzz

--- src/components/Projects/Practices/looping.js
+++ src/components/Projects/Practices/looping.js
@@ -1,8 +1,43 @@
 function looping() {
-    let loop = '.ì.';
+    let loop = '#';
 
     while (loop.length <= 7) {
         console.log(loop);
         loop = loop + "#"
     }
+}
+
+function fizz() {
+    for(let i = 1; i <= 100; i++) {
+        if(i % 15 === 0) {
+            console.log('FizzBuzz');
+        }else if(i % 3 === 0) {
+            console.log('Fizz'); 
+        }else if (i % 5 === 0){
+            console.log("Buzz");
+        }else {
+            console.log(i);
+        }
+    }
+}
+
+function buzz() {
+    for(let i = 1; i <= 101;) {
+        console.log(
+            (i++ % 3 ? '' : 'Fizz') +
+            (i % 5 ? '' : 'Buzz') || i
+        );
+    }
+}
+
+function chessboard () {
+    let output = '';
+    for (let i = 1; i <= 8; i++) {
+        for(let j = 1; j <= 8; j++) {
+            ((i + j) %2 == 0 ? output += ' ' : output += '#');
+        }
+        output += '\n';
+    }
+    console.log(output);
+
 }
