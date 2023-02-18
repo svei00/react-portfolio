aadcc54c8625b657c2f5a3dcdd28ddcceb904e35
svei00 <79055634+svei00@users.noreply.github.com>
2023-02-17 19:59:28 -0800

    added anothe fizzbuzz solution

--- src/components/Projects/Practices/looping.js
+++ src/components/Projects/Practices/looping.js
@@ -62,3 +62,12 @@ function pine() {
         console.log('\n');
     }
 }
+
+function fizzbuzz2() {
+    for (let i = 1; i <= 100; i++) {
+        let output = '';
+        if(i % 3 === 0) output += 'Fizz ';
+        if(i % 5 === 0) output += 'Buzz';
+        console.log(output || i);
+    }
+}
