7083b61a3b0c4dcb5757eb7667d09af30e756b55
svei00 <79055634+svei00@users.noreply.github.com>
2024-11-12 21:52:15 -0800

    Updating the Firebase authentification due the old version

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,8 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
-import { getStorage } from "firebase/storage";
-import { getFirestore } from 'firebase/firestore'
+import { initializeApp } from "firebase/app"
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth"
+import { getFirestore } from "firebase/firestore"
+import { getStorage } from "firebase/storage"
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -14,14 +14,22 @@ const firebaseConfig = {
   projectId: "portfolio-dashboard-c2761",
   storageBucket: "portfolio-dashboard-c2761.appspot.com",
   messagingSenderId: "294426353251",
-  appId: "1:294426353251:web:d7b0ef93e0ed0a1059a256"
-};
+  appId: "1:294426353251:web:d7b0ef93e0ed0a1059a256",
+}
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth();
-const provider = new GoogleAuthProvider();
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+const app = initializeApp(firebaseConfig)
+export const auth = getAuth(app)
+export const db = getFirestore(app)
+export const storage = getStorage(app)
 
-export const signInWithGoogle = () => signInWithPopup(auth, provider);
+export const signInWithGoogle = async () => {
+  const provider = new GoogleAuthProvider()
+  try {
+    await signInWithPopup(auth, provider)
+    // The onAuthStateChanged listener in Dashboard will handle any actions after sign-in
+  } catch (error) {
+    console.error("Error during sign-in:", error)
+    alert("Sign-in failed. Please try again.")
+  }
+}
