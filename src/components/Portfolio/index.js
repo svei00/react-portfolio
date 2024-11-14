6efa0f2ac9da7756c556427fdcef6d3fc43b3a7e
svei00 <79055634+svei00@users.noreply.github.com>
2024-11-13 19:49:59 -0800

    Improving the Portfolio page

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,102 +1,101 @@
-import React, { useEffect, useRef, useState } from 'react';
-import Loader from 'react-loaders';
-import AnimatedLetters from '../AnimatedLetters';
-import './index.scss';
-import windowLogo from '../../assets/images/LogoExcelWindow.png'
-// import portfolioData from '../../data/portfolio.json'; // Staticdata
-import { getDocs, collection } from 'firebase/firestore';
-import { db } from '../../firebase';
-import { Link } from 'react-router-dom';
-import { Helmet } from 'react-helmet';
-
+import React, { useEffect, useState } from "react"
+import Loader from "react-loaders"
+import AnimatedLetters from "../AnimatedLetters"
+import "./index.scss"
+import windowLogo from "../../assets/images/LogoExcelWindow.png"
+import { getDocs, collection } from "firebase/firestore"
+import { db } from "../../firebase"
+import { Link } from "react-router-dom"
+import { Helmet } from "react-helmet"
 
 const Portfolio = () => {
+  const [letterClass, setLetterClass] = useState("text-animate")
+  const [portfolio, setPortfolio] = useState([])
 
-    const [letterClass, setLetterClass] = useState('text-animate');
-    const [portfolio, setPortfolio] = useState([]);                 // To update the array
-    const dashboard = useRef('./dashboard')
+  // Animate letters on page load
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLetterClass("text-animate-hover")
+    }, 3000)
 
-    useEffect (() => {
-        const timer = setTimeout(() => {
-            setLetterClass('text-animate-hover');
-        }, 3000);
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [])
 
-        return () => {
-            clearTimeout(timer);
-        }
-    })
-
-    // Retriving data from Firebase
-    useEffect (() => {
-      getPortfolio();  
-    }, []);
-
-    // Get data from the server
+  // Fetch portfolio data from Firebase
+  useEffect(() => {
     const getPortfolio = async () => {
-        const querySnapshot = await getDocs(collection(db, 'portfolio')); // Name of the collection in Firebase.
-        console.log(querySnapshot); // Verify if we can retrieve data fron server
-        setPortfolio(querySnapshot.docs.map((doc) => doc.data()));                        
-    };
+      try {
+        const querySnapshot = await getDocs(collection(db, "portfolio"))
+        setPortfolio(querySnapshot.docs.map((doc) => doc.data()))
+      } catch (error) {
+        console.error("Error fetching portfolio data: ", error)
+      }
+    }
+    getPortfolio()
+  }, [])
 
-    console.log(portfolio); // Test 
+  const handleViewButtonClick = (url) => {
+    window.open(url, "_blank")
+  }
 
-    const renderPortfolio = (portfolio) => {
-        return (
-            <div className='images-container'>
-                <Helmet>
-                    <title>Portfolio | Ivan E. Villanueva</title>
-                    <meta 
-                        name='descriprion' content='Bilingual Software Engineer'
-                    />
-                    <meta 
-                        name='keywords' content='Software Engineer, Bilingual, English, Spanish'
-                    />
-                </Helmet>
-                {
-                    portfolio.map((port, idx) => {
-                        return (
-                            <div className='image-box' key={idx}>
-                                <img 
-                                    src={port.image}            // Was cover in mock
-                                    className='portfolio-image'
-                                    alt="portfolio"
-                                />
-                                <div className='content'>
-                                    <p className='title'>{port.name}</p> {/* // Was title in mock data*/}
-                                    <h4 className='description'>{port.description}</h4>
-                                    <button
-                                        className='btn'
-                                        onClick={() => window.open(port.url)}
-                                    >View</button>
-                                </div>
-                            </div>
-                        )
-                    })
-                }
-                <Link to='/dashboard'>
-                    <img className='loginDashboard' ref={dashboard} src={windowLogo} alt='Excel SolutionsV' />
-                </Link>
-            </div>
-        );
-    } 
-    
-    return (
-        <>
-            <div className='container portfolio-page'>
-                <h1 className='page-title'>
-                    <AnimatedLetters 
-                        letterClass={letterClass}
-                        strArray={'Portfolio'.split('')}
-                        idx={15}                                                // Remember that 15 means 1.5 and it is the delay from starting loading the page
-                    />
-                </h1>
-                <div>
-                    {renderPortfolio(portfolio)}                     {/* We use this (portfolioData) mock since we don't have a real databasa */}
-                </div>
-            </div>
-            <Loader type='pacman' />
-        </>
-    );
+  const renderPortfolio = () => (
+    <div className="images-container">
+      <Helmet>
+        <title>Portfolio | Ivan E. Villanueva</title>
+        <meta name="description" content="Bilingual Software Engineer" />
+        <meta
+          name="keywords"
+          content="Software Engineer, Bilingual, English, Spanish"
+        />
+      </Helmet>
+      {portfolio.map((port, idx) => (
+        <div className="image-box" key={idx}>
+          <img
+            src={port.image}
+            className="portfolio-image"
+            alt={port.name || "portfolio"}
+            loading="lazy" // Lazy loading for better performance
+            onError={(e) => (e.target.src = "/path/to/fallback-image.png")} // Fallback image in case of error
+          />
+          <div className="content">
+            <p className="title">{port.name}</p>
+            <h4 className="description">{port.description}</h4>
+            <button
+              className="btn"
+              onClick={() => handleViewButtonClick(port.url)}
+            >
+              View
+            </button>
+          </div>
+        </div>
+      ))}
+      <Link to="/dashboard">
+        <img
+          className="loginDashboard"
+          src={windowLogo}
+          alt="Excel SolutionsV"
+        />
+      </Link>
+    </div>
+  )
+
+  return (
+    <>
+      <div className="container portfolio-page">
+        <h1 className="page-title">
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={"Portfolio".split("")}
+            idx={15}
+          />
+        </h1>
+        <div>{renderPortfolio()}</div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
 }
 
-export default Portfolio;
+export default Portfolio
